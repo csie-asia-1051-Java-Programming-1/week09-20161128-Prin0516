9acfaf340aad6b97556c1097bbc5a9696c2a968d
Prin0516 <105021046@live.asia.edu.tw>
2016-11-28 16:00:21 +0800

    ex

--- src/ex/ex02.java
+++ src/ex/ex02.java
@@ -7,11 +7,68 @@
  * Author: 1050210XX 周永振老師
  */
 
+
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		ArrayList<Student> studList = new ArrayList<Student>();
+		studList.add(new Student("David", "1899"));
+		studList.add(new Student("Jack", "1085"));
+		studList.add(new Student("Kevin", "1985"));
+		studList.get(0).showInfo();
+		studList.get(0).addCourse("程式一", "1050807");
+		studList.get(0).showInfo();
 
+	}
+
+}
+
+class Student {
+	private String id, name;
+	private ArrayList<Course> CouList = new ArrayList<Course>();
+
+	public Student(String Name1, String id1) {
+		name = Name1;
+		id = id1;
+	}
+
+	public void showInfo() {
+		System.out.println("name:" + name);
+		System.out.println("id:" + id);
+		System.out.println("#of Course:" + CouList.size());
+		for (int i = 0; i < CouList.size(); i++) {
+			System.out.println("course name:" + CouList.get(i).getName());
+			System.out.println("ID:" + CouList.get(i).getID());
+		}
+
+	}
+
+	public void addCourse(String name1, String id1) {
+		CouList.add(new Course(name1, id1));
+	}
+}
+
+    class Course{
+	private String couName, couID, teacher;
+	private int score;
+
+	public Course(String name1, String id1) {
+		couName = name1;
+		couID = id1;
+	}
+
+	public String getName() {
+		return couName;
+
+	}
+
+	public String getID() {
+		return couID;
 	}
 
 }
